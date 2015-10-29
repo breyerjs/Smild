b79c9437439e5bad808a37d2f438c19eff1bc819
Jackson Breyer <breyer.js@gmail.com>
2015-10-29 15:59:36 -0400

    more files for the intro tour

---
+++ OneGoodThing/app/src/main/java/com/onegoodthing/onegoodthing/IntroTour.java
@@ -0,0 +1,19 @@
+package com.onegoodthing.onegoodthing;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jackson Breyer on 10/29/2015.
+ */
+public class IntroTour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_tour);
+    }
+
+
+}
+
